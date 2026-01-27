91b11614baddc0fa753fa19c982716ba671f1a57
cosmuslum <cosmuslum@gmail.com>
2026-01-27 13:57:26 +0100

    Create page.tsx

---
+++ app/ar/page.tsx
@@ -0,0 +1,9 @@
+export default function ArHome() {
+  return (
+    <main dir="rtl" style={{ padding: 32, fontFamily: "system-ui" }}>
+      <h1>NederLearn</h1>
+      <p>تعلّم الهولندية خطوة بخطوة 🇳🇱</p>
+      <p><a href="/tr">العودة إلى الصفحة التركية</a></p>
+    </main>
+  );
+}
