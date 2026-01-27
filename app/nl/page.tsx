641032959d1b5a6a5d4e8877bb49406f952d4854
cosmuslum <cosmuslum@gmail.com>
2026-01-27 13:55:44 +0100

    Create page.tsx

---
+++ app/nl/page.tsx
@@ -0,0 +1,9 @@
+export default function NlHome() {
+  return (
+    <main style={{ padding: 32, fontFamily: "system-ui" }}>
+      <h1>NederLearn</h1>
+      <p>Leer Nederlands stap voor stap 🇳🇱</p>
+      <p><a href="/tr">← Türkçe ana sayfa</a></p>
+    </main>
+  );
+}
