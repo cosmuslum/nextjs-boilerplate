1cf7d5200fa89e08d88ed0a8ed90552576ef1f78
cosmuslum <cosmuslum@gmail.com>
2026-01-28 20:50:01 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,17 @@
 import "./globals.css";
+import Providers from "./providers";
+
+export const metadata = {
+  title: "NederLearn",
+  description: "Hollandaca öğren",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="tr">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
